3220f581b6b110fe94a30800cd85142b1bca0b44
clement brossier <brossier.clement@proton.me>
2023-11-23 01:03:06 +0100

    fix: switch vod for img

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Home from "./assets/pages/home/home";
 import Projects from "./assets/pages/projects/projects";
 import Description from "./assets/pages/description/description";
 import Footer from "./assets/components/footer/footer";
-import video from "./assets/videos/cyberpunk.mp4";
+// import video from "./assets/videos/cyberpunk.mp4";
+import background from "../src/assets/images/background.png";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <video className="vod" src={video}></video>
+      {/* <video className="vod" src={video}></video> */}
+      <img className="vod" src={background} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
